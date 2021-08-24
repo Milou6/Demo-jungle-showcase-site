223d6aecf7b67ba6740a472e4e59b3891ac887c3
Milou6 <haas_emile@hotmail.com>
2021-08-24 11:15:12 +0200

    Animating section 2, added Footer

--- animations.js
+++ animations.js
@@ -1,22 +1,40 @@
-// let options = {
-//   root: null,
-//   rootMargin: '0px',
-//   threshold: 1.0
-// }
-
 const inViewport = (entries, observer) => {
   entries.forEach((entry) => {
-    entry.target.classList.toggle('is-inViewport', entry.isIntersecting)
-    // entries.target.style.opacity = '1'
+    // console.log(entry)
+    // console.log(entry.intersectionRatio)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('is-inViewport')
+    }
   })
 }
 
-const Obs = new IntersectionObserver(inViewport)
-const obsOptions = {} //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+//See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+const obsOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1
+}
 
-// Attach observer to every [data-inviewport] element:
-const ELs_inViewport = document.querySelectorAll('#grid-title')
-console.log(ELs_inViewport)
+const Obs = new IntersectionObserver(inViewport, obsOptions)
+
+// Attach observer to section2 title:
+const ELs_inViewport = document.querySelectorAll('.grid-title')
+// console.log(ELs_inViewport)
 ELs_inViewport.forEach((EL) => {
-  Obs.observe(EL, obsOptions)
+  Obs.observe(EL)
+})
+
+const inViewport2 = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('detail-showing')
+    }
+  })
+}
+
+const Obs2 = new IntersectionObserver(inViewport2, obsOptions)
+
+const ELs_inViewport2 = document.querySelectorAll('.detail')
+ELs_inViewport2.forEach((EL) => {
+  Obs2.observe(EL)
 })
